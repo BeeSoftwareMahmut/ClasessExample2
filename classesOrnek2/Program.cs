9971b56e51d25c66558373bedcc1c39310ca488b
Mahmut_Ari <arimahmut2314@gmail.com>
2023-04-18 22:14:11 +0300

    Paylaşılacak dosyaların hazırlanması

---
+++ classesOrnek2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesOrnek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringClass value = new StringClass();
+            Console.Write("Enter a string value : ");
+            string EnterEdValue = value.assignString();
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Lower Case : {0}",value.convertToLower(EnterEdValue));
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Upper Case : {0}", value.convertToUpper(EnterEdValue));
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Vice Versa :{0}",value.convertViceVersa(EnterEdValue));
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("First Leter Of words :{0}",value.firstLetter(EnterEdValue));
+            Console.WriteLine("----------------------------");
+            value.printReverse(EnterEdValue);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ classesOrnek2/StringClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesOrnek2
+{
+    class StringClass
+    {
+        string value;
+
+        public string assignString()
+        {
+            value = Console.ReadLine();
+            return value;
+        }
+        public string convertToLower(string value)
+        {
+            return value.ToLower();
+        }
+        public string convertToUpper(string value)
+        {
+            return value.ToUpper();
+        }
+        public StringClass()
+        {
+
+        }
+        public string convertViceVersa(string value)
+        {
+            string valueOfViceVersa = "";
+            foreach(var x in value)
+            {
+                if (char.IsUpper(x))
+                {
+                    valueOfViceVersa+=x.ToString().ToLower();
+                }
+                else
+                {
+                    valueOfViceVersa += x.ToString().ToUpper();
+                }
+
+            }
+            return valueOfViceVersa;
+        }
+
+        public string firstLetter(string value)
+        {
+            string firstLetterArray = value[0].ToString().ToUpper()+" ";
+            for(int i=0;i<value.ToString().Trim().Length; i++)
+            {
+                if (value[i].ToString() == " ")
+                {
+                    firstLetterArray += value[i + 1].ToString().ToUpper()+" ";
+                }
+            }
+
+            return firstLetterArray;
+        }
+        public void printReverse(string value)
+        {
+            string reverse = "";
+           for(int i = value.Length - 1; i >= 0; i--)
+            {
+                reverse += value[i].ToString();
+            }
+            Console.WriteLine("Reverse of value:{0} ", reverse);
+        }
+
+    }
+}
